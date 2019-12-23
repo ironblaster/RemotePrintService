748442563302ba6294d474ac803bbf77d987fcaf
ironblaster <ironblaster@users.noreply.github.com>
2019-12-23 12:32:14 +0100

    BugFixA4

--- src/main/java/main/PrintersFunction.java
+++ src/main/java/main/PrintersFunction.java
@@ -13,9 +13,11 @@ import javax.print.DocPrintJob;
 import javax.print.PrintException;
 import javax.print.PrintService;
 import javax.print.SimpleDoc;
+import javax.print.attribute.DocAttributeSet;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.MediaSizeName;
 
 public class PrintersFunction {
 	
@@ -55,10 +57,10 @@ public class PrintersFunction {
 		             DocFlavor flavor =  DocFlavor.INPUT_STREAM.AUTOSENSE;
 
 		            
-		          //   PrintRequestAttributeSet  pras = new HashPrintRequestAttributeSet();
+		             PrintRequestAttributeSet  pras = new HashPrintRequestAttributeSet();
 		             //pras.add(new JobName("Lavoro_1", Locale.ITALIAN));
 		             //pras.add(OrientationRequested.PORTRAIT);
-		             //pras.add(MediaSizeName.ISO_A4);
+		             pras.add(MediaSizeName.ISO_A4);
 		            // pras.add(new Copies(copie));
 		             //pras.add(Sides.TWO_SIDED_LONG_EDGE);
 		             //pras.add(Finishings.STAPLE);
@@ -76,7 +78,7 @@ public class PrintersFunction {
 		           
 
 		             // Print it
-		             job.print(doc, null);
+		             job.print(doc, pras);
 		             is.close();
 
 		 }
